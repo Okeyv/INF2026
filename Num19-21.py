7b40079743dd3c31ac3ec513caad3b43d58d0ff1
Okeyv <okeyvv@outlook.com>
2026-04-23 15:24:42 +0400

    Fix

--- Num19-21.py
+++ Num19-21.py
@@ -2,14 +2,14 @@
 
 # Базовый код для одной куче камней
 
-def f(s,m):
-    if s<16 :return m%2==0
-    if m == 0:return 0
-    h = [f(s-3,m-1),f(s-8,m-1),f(s//3,m-1)]
-    return any(h) if m%2!=0 else all(h)
-print("19",*[s for s in range(16,300)if f(s,2)])
-print("20",*[s for s in range(16,300)if not f(s,1)and f(s,3)])
-print("21",*[s for s in range(16,300)if not f(s,2)and f(s,4)])
+# def f(s,m):
+#     if s<16 :return m%2==0
+#     if m == 0:return 0
+#     h = [f(s-3,m-1),f(s-8,m-1),f(s//3,m-1)]
+#     return any(h) if m%2!=0 else all(h)
+# print("19",*[s for s in range(16,300)if f(s,2)])
+# print("20",*[s for s in range(16,300)if not f(s,1)and f(s,3)])
+# print("21",*[s for s in range(16,300)if not f(s,2)and f(s,4)])
 
 # Меняется только значения в 'h', но в ней переменная 'm-1', всегда такая.
 # А так же меняется условие if "s<16"
